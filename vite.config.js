9ffbd91fc47ee6e879258017c03292e2a4da6039
Bridger Anderson <bridgerdean47@gmail.com>
2025-12-12 16:20:56 +0000

    "added logo"

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/budget-app/", // important for GitHub Pages
+  base: "/BudgetR/", // important for GitHub Pages
   plugins: [react()],
   build: {
     // just raise the limit so the 500 kB warning goes away
